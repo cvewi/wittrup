b9c9576261cbbed1e9f823983de8f30fa2287ef0
cvewi <66957160+cvewi@users.noreply.github.com>
2020-08-12 11:36:21 +0200

    Change from Hugo to std html, css and JS setup

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function contactbox() {
+    var x = document.getElementById("footer-contact");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
